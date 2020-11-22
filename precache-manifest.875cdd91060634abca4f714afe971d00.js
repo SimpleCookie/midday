8fd2f0e305e1367bb9bbb5d3ec85cf62840dd57b
Devgroup <marcus.ekstrom@devgroup.se>
2020-11-22 14:47:00 +0100

    Updates

---
+++ precache-manifest.875cdd91060634abca4f714afe971d00.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "485e4347d57d2786498d",
+    "url": "/midday/static/css/main.09161eb1.chunk.css"
+  },
+  {
+    "revision": "485e4347d57d2786498d",
+    "url": "/midday/static/js/main.485e4347.chunk.js"
+  },
+  {
+    "revision": "4e01a3bea31673e6fbaf",
+    "url": "/midday/static/js/runtime~main.4e01a3be.js"
+  },
+  {
+    "revision": "42a0381f7a1d7f1b1379",
+    "url": "/midday/static/js/2.42a0381f.chunk.js"
+  },
+  {
+    "revision": "4aacfd49e2e066e35841bdc59f6b81c9",
+    "url": "/midday/index.html"
+  }
+];
